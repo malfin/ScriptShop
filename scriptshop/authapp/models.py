3f28dc18c71fca91db24b9dbfc38a71f73757251
Алексей <aiex.0115970@gmail.com>
2021-09-05 18:53:45 +0500

    edit models.py

--- scriptshop/authapp/models.py
+++ scriptshop/authapp/models.py
@@ -4,11 +4,12 @@ from django.db import models
 
 
 class UserProfile(AbstractUser):
-    img = models.ImageField(verbose_name='аватарка', upload_to='userprofile_avatars', blank=True)
-    balance = models.DecimalField(verbose_name='баланс', max_digits=6, decimal_places=2, default=0)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{7,15}$',
                                  message="Номер телефона необходимо вводить в формате: '+799999999'. Допускается до "
                                          "15 цифр.")
+
+    img = models.ImageField(verbose_name='аватарка', upload_to='userprofile_avatars', blank=True)
+    balance = models.DecimalField(verbose_name='баланс', max_digits=6, decimal_places=2, default=0)
     phone_number = models.CharField(verbose_name='номер телефона', validators=[phone_regex], max_length=17, blank=True)
 
     def __str__(self):
