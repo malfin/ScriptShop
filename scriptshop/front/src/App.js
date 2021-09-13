44786a38304dff42f7fd7c8eeef3e70f8ae3eccd
Алексей <aiex.0115970@gmail.com>
2021-09-13 10:44:47 +0500

    Edit App.js + edit db.sqlite3

--- scriptshop/front/src/App.js
+++ scriptshop/front/src/App.js
@@ -1,51 +1,70 @@
-import './App.css';
+// import './App.css';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 
 
-function App() {
-    return (
-        <div className="container">
-            <Header/>
-            <h3 className="mt-5">Все Товары</h3>
-            <div className="App m-5">
-                <div className="d-flex justify-content-between flex-wrap">
-                    <div className="card">
-                        <div className="card-header">
-                            <h5>Товар #1</h5>
-                        </div>
-                        <div className="card-body">
-                            <p>
-                                Описание
-                            </p>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card-header">
-                            <h5>Товар #2</h5>
-                        </div>
-                        <div className="card-body">
-                            <p>
-                                Описание
-                            </p>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="card-header">
-                            <h5>Товар #3</h5>
-                        </div>
-                        <div className="card-body">
-                            <p>
-                                Описание
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Footer/>
-        </div>
-    );
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.setState({
+            'name': [],
+            'category': [],
+            'sum': [],
+        });
+    }
+    render() {
+        console.log('state', this.state);
+        return(
+            <div>ScriptShop</div>
+        )
+    }
 }
+
+
+// function App() {
+//     return (
+//         <div className="container">
+//             <Header/>
+//             <h3 className="mt-5">Все Товары</h3>
+//             <div className="App m-5">
+//                 <div className="d-flex justify-content-between flex-wrap">
+//                     <div className="card">
+//                         <div className="card-header">
+//                             <h5>Товар #1</h5>
+//                         </div>
+//                         <div className="card-body">
+//                             <p>
+//                                 Описание
+//                             </p>
+//                         </div>
+//                     </div>
+//                     <div className="card">
+//                         <div className="card-header">
+//                             <h5>Товар #2</h5>
+//                         </div>
+//                         <div className="card-body">
+//                             <p>
+//                                 Описание
+//                             </p>
+//                         </div>
+//                     </div>
+//                     <div className="card">
+//                         <div className="card-header">
+//                             <h5>Товар #3</h5>
+//                         </div>
+//                         <div className="card-body">
+//                             <p>
+//                                 Описание
+//                             </p>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//             <Footer/>
+//         </div>
+//     );
+// }
 
 export default App;
