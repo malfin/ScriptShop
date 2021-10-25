0dd3b5b1c8ffd8487c66f8d1caad12fcfac90d6c
Алексей <aiex.0115970@gmail.com>
2021-10-25 09:09:26 +0500

    homework before the start of the hackathon

--- scriptshop/front/src/App.js
+++ scriptshop/front/src/App.js
@@ -11,183 +11,6 @@ import axios from "axios";
 import CategoryDetail from "./components/CategoryDetail";
 import UserDetail from "./components/UserDetail";
 
-const userMock = [
-    {
-        "id": 11,
-        "first_name": "",
-        "last_name": "",
-        "username": "admin",
-        "balance": "9999.00",
-        "phone_number": ""
-    },
-    {
-        "id": 82,
-        "first_name": "Gabriel",
-        "last_name": "Hensley",
-        "username": "ricky50",
-        "balance": "735.00",
-        "phone_number": "KdNeSKJXTcbPnVKUX"
-    },
-    {
-        "id": 83,
-        "first_name": "Chelsey",
-        "last_name": "Choi",
-        "username": "johnabbott",
-        "balance": "828.00",
-        "phone_number": "dvdlBEXrxzJfTxEAj"
-    },
-    {
-        "id": 84,
-        "first_name": "Summer",
-        "last_name": "Scott",
-        "username": "gordon88",
-        "balance": "56.00",
-        "phone_number": "BqMNPQmFQxJIOIUmU"
-    },
-    {
-        "id": 85,
-        "first_name": "Eileen",
-        "last_name": "Nguyen",
-        "username": "max68",
-        "balance": "973.00",
-        "phone_number": "UemSUFAygQeMpbCbU"
-    },
-    {
-        "id": 86,
-        "first_name": "Jake",
-        "last_name": "George",
-        "username": "evansmia",
-        "balance": "749.00",
-        "phone_number": "InKjTVYrzZuVGsyIZ"
-    },
-    {
-        "id": 87,
-        "first_name": "Max",
-        "last_name": "Lowery",
-        "username": "gregg70",
-        "balance": "670.00",
-        "phone_number": "YFSlEFIwjUhekDHuI"
-    },
-    {
-        "id": 88,
-        "first_name": "Joe",
-        "last_name": "Roy",
-        "username": "michaelaandrade",
-        "balance": "480.00",
-        "phone_number": "xIwgBnNlHhDeiMcBf"
-    },
-    {
-        "id": 89,
-        "first_name": "Debra",
-        "last_name": "Ritter",
-        "username": "alvarezbeverly",
-        "balance": "157.00",
-        "phone_number": "vzBPuItRKUqAsIkCt"
-    },
-    {
-        "id": 90,
-        "first_name": "Charlene",
-        "last_name": "Johns",
-        "username": "clarkenoah",
-        "balance": "959.00",
-        "phone_number": "hIscZDXsSYnWvDNps"
-    },
-    {
-        "id": 91,
-        "first_name": "Donna",
-        "last_name": "Montgomery",
-        "username": "brandy53",
-        "balance": "243.00",
-        "phone_number": "mxjVmjIZKItuPbmTT"
-    }
-];
-
-const productMock = [
-    {
-        "id": 1,
-        "name": "Товар #1",
-        "desc": "Описание товара, товар #1",
-        "sum": "58.00",
-        "img": null,
-        "created": "2021-09-04T19:16:31.611064+05:00",
-        "updated": "2021-09-04T19:16:31.611064+05:00",
-        "category": 1
-    },
-    {
-        "id": 2,
-        "name": "Товар #2",
-        "desc": "Описание товара, товар #2",
-        "sum": "49.00",
-        "img": null,
-        "created": "2021-09-04T19:16:31.627060+05:00",
-        "updated": "2021-09-04T19:16:31.627060+05:00",
-        "category": 2
-    },
-    {
-        "id": 3,
-        "name": "Товар #3",
-        "desc": "Описание товара, товар #3",
-        "sum": "4.00",
-        "img": null,
-        "created": "2021-09-04T19:16:31.641072+05:00",
-        "updated": "2021-09-04T19:16:31.642071+05:00",
-        "category": 3
-    },
-    {
-        "id": 4,
-        "name": "Товар #4",
-        "desc": "Описание товара, товар #4",
-        "sum": "56.00",
-        "img": null,
-        "created": "2021-09-04T19:16:31.657046+05:00",
-        "updated": "2021-09-04T19:16:31.657046+05:00",
-        "category": 4
-    },
-    {
-        "id": 5,
-        "name": "Товар #5",
-        "desc": "Описание товара, товар #5",
-        "sum": "36.00",
-        "img": null,
-        "created": "2021-09-04T19:16:31.672047+05:00",
-        "updated": "2021-09-04T19:16:31.672047+05:00",
-        "category": 5
-    }
-];
-
-const categoryMock = [
-    {
-        "id": 1,
-        "name": "Категория #1",
-        "created": "2021-09-04T19:12:39.328876+05:00",
-        "updated": "2021-09-04T19:12:39.328876+05:00"
-    },
-    {
-        "id": 2,
-        "name": "Категория #2",
-        "created": "2021-09-04T19:12:39.343870+05:00",
-        "updated": "2021-09-04T19:12:39.343870+05:00"
-    },
-    {
-        "id": 3,
-        "name": "Категория #3",
-        "created": "2021-09-04T19:12:39.355867+05:00",
-        "updated": "2021-09-04T19:12:39.355867+05:00"
-    },
-    {
-        "id": 4,
-        "name": "Категория #4",
-        "created": "2021-09-04T19:12:39.368860+05:00",
-        "updated": "2021-09-04T19:12:39.368860+05:00"
-    },
-    {
-        "id": 5,
-        "name": "Категория #5",
-        "created": "2021-09-04T19:12:39.380859+05:00",
-        "updated": "2021-09-04T19:12:39.380859+05:00"
-    }
-];
-
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -201,17 +24,32 @@ class App extends React.Component {
     componentDidMount() {
         // call rest API
         axios
-            .get("http://localhost:8000/api/v1/users/")
+            .get("http://127.0.0.1:8000/api/v1/users/")
             .then((result) => {
-                console.log('result', result)
+                // console.log('users result:', result)
+                this.setState({
+                    users: result.data
+                })
             })
             .catch((error) => console.log(error));
-        this.setState({
-                products: productMock,
-                categories: categoryMock,
-                users: userMock,
-            }
-        )
+        axios
+            .get("http://127.0.0.1:8000/api/v1/products/")
+            .then((result) => {
+                // console.log('users result:', result)
+                this.setState({
+                    products: result.data
+                })
+            })
+            .catch((error) => console.log(error));
+        axios
+            .get("http://127.0.0.1:8000/api/v1/category/")
+            .then((result) => {
+                // console.log('users result:', result)
+                this.setState({
+                    category: result.data
+                })
+            })
+            .catch((error) => console.log(error));
     }
 
     render() {
@@ -232,7 +70,7 @@ class App extends React.Component {
                     <Route exact path="/users/detail/:id">
                         <UserDetail users={this.state.users}/>
                     </Route>
-                     <Route exact path="/products/">
+                    <Route exact path="/products/">
                         <ProductList products={this.state.users}/>
                     </Route>
                 </Router>
