1aab2ba82ce07937db970fa668cacbaf21657e45
Алексей <aiex.0115970@gmail.com>
2022-02-10 22:37:17 +0500

    deploy

--- production/backend/scriptshop/settings.py
+++ production/backend/scriptshop/settings.py
@@ -119,8 +119,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authapp.UserProfile'
 CORS_ALLOWED_ORIGINS = [
-    # 'http://127.0.0.1:3000',
-    'http://localhost:3000',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
 ]
 
 REST_FRAMEWORK = {
