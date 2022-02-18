59c0f9af01e6525d6d58d70bb2bc002003ef9043
Алексей <aiex.0115970@gmail.com>
2022-02-18 20:09:17 +0500

    update and add db

--- backend/scriptshop/settings.py
+++ backend/scriptshop/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'scriptshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scriptshop',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'POST': 5432,
     }
 }
 
