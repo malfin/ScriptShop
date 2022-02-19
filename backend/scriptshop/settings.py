2f76bfb19d465cefa6580dba925f07d6dbabb64c
Алексей <aiex.0115970@gmail.com>
2022-02-19 08:56:51 +0500

    Not work CorsHeader!

--- backend/scriptshop/settings.py
+++ backend/scriptshop/settings.py
@@ -123,8 +123,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authapp.UserProfile'
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000',
-    'http://localhost:8000',
+    'http://127.0.0.1:85',
+    'http://localhost:85',
 ]
 
 REST_FRAMEWORK = {
